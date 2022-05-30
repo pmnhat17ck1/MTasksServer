2658af6c1181e2d85b80a759ab96de638d6946b7
nhat <pmnhat.17ck1@gmail.com>
2022-05-30 22:07:44 +0700

    fix

--- models/index.js
+++ models/index.js
@@ -55,36 +55,36 @@ Priority.hasMany(Image);
 Image.belongsTo(Priority);
 
 
-const checkTable = async () => {
+// const checkTable = async () => {
 
-  const role = await Role?.countAll();
-  const country = await Country?.countAll();
-  const priority = await Priority?.countAll();
-  const step = await Step?.countAll();
-  const type = await Type?.countAll();
-  if (role?.length > 0 || country?.length > 0 || priority?.length > 0 || step?.length > 0 || type?.length > 0) {
-    return;
-  }
-  Country.add("Việt nam", "vi");
-  Country.add("US", "en");
-  Role.add("admin")
-  Role.add("member")
-  Priority.add("medium")
-  Priority.add("high")
-  Priority.add("highest")
-  Step.add("To do", "", "");
-  Step.add("In progress", "", "")
-  Step.add("Review", "", "")
-  Step.add("Done", "", "")
-  Type.add("Feature", "#4BADE8")
-  Type.add("Bug", "#E5493A")
-  const user = User.add('admin123', '$2b$10$XonxxewUUoY0IgxXj.hiPO4Gi8j5HKlF80IqMbIta9JfXmj23cU1W','111111111', 'admin123@gmail.com', true, 1, 1 )
-  const accessToken = await generateAccessToken({ user_id: user.id });
-  Detail.add(user?.id)
-  Token.add(user.id, accessToken)
-};
+//   const role = await Role?.countAll();
+//   const country = await Country?.countAll();
+//   const priority = await Priority?.countAll();
+//   const step = await Step?.countAll();
+//   const type = await Type?.countAll();
+//   if (role?.length > 0 || country?.length > 0 || priority?.length > 0 || step?.length > 0 || type?.length > 0) {
+//     return;
+//   }
+//   Country.add("Việt nam", "vi");
+//   Country.add("US", "en");
+//   Role.add("admin")
+//   Role.add("member")
+//   Priority.add("medium")
+//   Priority.add("high")
+//   Priority.add("highest")
+//   Step.add("To do", "", "");
+//   Step.add("In progress", "", "")
+//   Step.add("Review", "", "")
+//   Step.add("Done", "", "")
+//   Type.add("Feature", "#4BADE8")
+//   Type.add("Bug", "#E5493A")
+//   const user = User.add('admin123', '$2b$10$XonxxewUUoY0IgxXj.hiPO4Gi8j5HKlF80IqMbIta9JfXmj23cU1W','111111111', 'admin123@gmail.com', true, 1, 1 )
+//   const accessToken = await generateAccessToken({ user_id: user.id });
+//   Detail.add(user?.id)
+//   Token.add(user.id, accessToken)
+// };
 
-checkTable();
+// checkTable();
 
 module.exports = {
   Image,
